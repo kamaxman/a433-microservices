3a018980a25b65d791552d99790f9c16ef77d642
kamaxman <137511679+kamaxman@users.noreply.github.com>
2023-11-18 17:10:38 +0800

    Delete build_push_image.sh

--- build_push_image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker build -t item-app:v1 .
-docker images
-docker tag item-app:v1 kamaxman/item-app:v1
-echo $PASSWORD_DOCKER_HUB | docker login -u kamaxman --password-stdin
-docker push kamaxman/item-app:v1
- 
